2f2f078d57f9ce8c8e2bf30e58764d098f143f0a
eyang106 <eugenejyang@gmail.com>
2021-01-25 14:39:42 -0800

    This is a commit message.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def print_test():
 def add_eugene():
     print("Eugene Yang")
 
+def add_eugene_2():
+    print("Eugene Yang 2: Electric Boogaloo")
+
 def main():
     print("The participants in this workshop were: ")
     print_test()
     add_eugene()
+    add_eugene_2
     
 if __name__ == "__main__":
     main()
