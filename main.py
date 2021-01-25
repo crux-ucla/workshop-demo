b2a9d13973f378ccdc9047326ec58778fe64c1ac
Tiffany Chen <tiffanychenusa@g.ucla.edu>
2021-01-25 14:39:40 -0800

    this is a commit message

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ def print_test():
 def add_eugene():
     print("Eugene Yang")
 
+def add_tiffany():
+    print("TIFFANY")
+
 def main():
     print("The participants in this workshop were: ")
     print_test()
     add_eugene()
+    add_tiffany
     
 if __name__ == "__main__":
     main()
     
+
