0609c46d48896485abce062a381767533eda36b4
crux-ucla <74840249+crux-ucla@users.noreply.github.com>
2021-01-24 21:33:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def print_test():
+    print("test user")
+
 def main():
     print("The participants in this workshop were: ")
-
+    print_test()
+    
 if __name__ == "__main__":
     main()
     
