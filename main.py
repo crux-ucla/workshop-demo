d00f0af282dc4c05d70e0f188d0f799bc4c0893c
eyang106 <eugenejyang@gmail.com>
2021-01-24 21:34:43 -0800

    Add print_eugene

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def print_test():
     print("test user")
 
+def add_eugene():
+    print("Eugene Yang")
+
 def main():
     print("The participants in this workshop were: ")
     print_test()
+    add_eugene()
     
 if __name__ == "__main__":
     main()
