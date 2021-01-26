e0356cce5c6018119d63a94dc5045e4f55909378
Manya Bali <61367163+manyabali@users.noreply.github.com>
2021-01-25 19:32:26 -0800

    Added Manya's Name

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ def add_eugene():
 def add_tiffany():
     print("TIFFANY")
 
+def add_manya():
+    print("Manya")
+
 def main():
     print("The participants in this workshop were: ")
     print_test()
     add_eugene()
     add_tiffany()
+    add_manya()
     
 if __name__ == "__main__":
     main()
