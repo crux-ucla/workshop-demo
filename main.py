92b5bd8fa1dea42aab486216587a39cc4718e18e
Martin  Bourdev <martinbourdev@Martins-MacBook-Air.local>
2021-01-26 18:24:25 -0800

    Added print_martin function

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ def add_tiffany():
 def add_manya():
     print("Manya")
 
+def add_martin():
+    print("Martin")
+
 def main():
     print("The participants in this workshop were: ")
     print_test()
     add_eugene()
     add_tiffany()
     add_manya()
+    add_martin()
     
 if __name__ == "__main__":
     main()
